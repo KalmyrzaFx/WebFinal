0a4f04c38cff0f0d540cc43b6d537b3e91dd16d1
Kalmyrza <Zholchubekovk@gmail.com>
2022-05-27 23:10:25 +0600

    async

--- server/index.js
+++ server/index.js
@@ -7,4 +7,18 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+
+start()
+
+
+
