4c7107af00ae960480b02adc67ff1740f61009f0
Kalmyrza <Zholchubekovk@gmail.com>
2022-06-03 22:46:10 +0600

    TypeBar

---
+++ client/src/components/TypeBar.js
@@ -0,0 +1,14 @@
+import { observer } from 'mobx-react-lite'
+import React from 'react'
+import { ListGroup } from 'react-bootstrap';
+import { Context } from '..';
+
+const TypeBar = observer(() => {
+    return (
+        <div>
+            
+        </div>
+    )
+});
+
+export default TypeBar
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,29 +1,48 @@
 import React from 'react'
-import { Button, Card, Container, Form } from 'react-bootstrap'
+import { Button, Card, Container, Form, Row } from 'react-bootstrap'
+import { NavLink, useLocation } from 'react-router-dom';
 import '../style/Style.css';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 
 const Auth = () => {
+
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE
+  console.log(location)
+
   return (
     <Container 
         className='d-flex justify-content-center align-items-center'
         style={{height: window.innerHeight - 54}}
     >
-        <Card style={{width: 800}}>
-            <h2 className='m-auto'>Authorization</h2>
+        <Card style={{width: 500,
+        height: 350}}>
+            <h2 className='m-auto'>{isLogin ? 'Authorization' : 'Registration'}</h2>
             <Form className="d-flex flex-column">
                 <Form.Control
-                    className='mt-4'
+                    className='mt-4 loginInp'
                     placeholder='Enter your email'
                 />
                 <Form.Control
-                    className='mt-4'
+                    className='mt-4 loginInp'
                     placeholder='Enter password'
                 />
-                <Button
-                    variant="dark" 
-                    className="loginBtn">
-                    Login
-                </Button>
+                <Row className="d-flex justify-content-between">
+                    {isLogin ?
+                        <div className='ms-5 mt-2'>
+                            New here <NavLink to={REGISTRATION_ROUTE}>registrate</NavLink>
+                        </div>
+                        :
+                        <div className='ms-5 mt-2'>
+                            Have account <NavLink to={LOGIN_ROUTE}>login</NavLink>
+                        </div>  
+                    }   
+                    <Button
+                        variant="dark" 
+                        className="loginBtn">
+                        {isLogin ? 'Login' : 'Registrate'}
+                    </Button>
+                </Row>
             </Form>
         </Card>
     </Container>
